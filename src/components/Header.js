44fbc54691b0c115767439865e3bc138aada4277
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-12-10 17:17:08 -0500

    Adding form
    Adding form

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import img1 from '../cineBuddyLogo.png'
+// import img1 from '../cineBuddyLogo.png'
 import img2 from '../cblogo11.png'
 
 const Header = () => {
   return (
-    <div className="absolute px-6 py-2 bg-gradient-to-b from-black">
+    <div className="absolute px-6 py-2 bg-gradient-to-b from-black z-10">
       
       <img className="w-52"
       src={img2} alt="logo" />
--- src/components/Login.js
+++ src/components/Login.js
@@ -5,10 +5,17 @@ const Login = () =>
     return (
         <div>
             <Header />
-            <div>
-            <img src='https://user-images.githubusercontent.com/33485020/108069438-5ee79d80-7089-11eb-8264-08fdda7e0d11.jpg'
-      alt='Background' />
-      </div>
+            <div className="absolute">
+                <img src='https://user-images.githubusercontent.com/33485020/108069438-5ee79d80-7089-11eb-8264-08fdda7e0d11.jpg'
+                     alt='Background' />
+            </div>
+
+      <form className="relative p-16 bg-transparent">
+        <input type="text" placeholder="Email-address" className="p-2 m-2" />
+        <input type="password" placeholder="Password" className="p-2 m-2" />
+
+        <button className="p-4 m-4 bg-rose-700"> Sign Up</button>
+      </form>
         </div>
         
     )
