59d275e399722e4f8e98fac3a573d6cb238b94e3
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-12-10 15:39:25 -0500

    Updated body with routing

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import Login from './Login'
+import Browse from './Browse'
+import { createBrowserRouter } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
 
 const Body = () => {
+
+    const appRouter = createBrowserRouter(
+        [
+            {
+                path: "/", 
+                element: <Login />
+            },
+            {
+                path: "/browse",
+                element: <Browse />
+            }
+        ]
+    )
   return (
     <div>
         <div className="text-4xl font-bold text-fuchsia-300">
       Namaste Everyone, Let's build Netflix GPT! 
+    </div>
+    <div>
+        <RouterProvider router={appRouter} />
     </div>
       
     </div>
