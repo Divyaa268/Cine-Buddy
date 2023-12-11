d16dc6a4496cc5c47eade5a2727c1e95cbf14e3c
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-12-11 02:23:10 -0500

    Using UseRef hook for email and password validation
    Using UseRef hook for email and password validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,28 @@
-import { useState } from "react"
+import { useState, useRef } from "react"
 import Header from "./Header"
+import validate from "../utils/validate";
 
 const Login = () =>
 {
     const [isSignInForm, setIsSignInForm] = useState(true);
 
+    const email = useRef(null);
+    const password = useRef(null);
+
     const toggleSignIn = () => {
         setIsSignInForm(!isSignInForm);
+
+    }
+
+
+    const handleButtonClick = () => {
+        // validate the data of the form
+
+        console.log(email.current.value);
+        console.log(password.current.value);
+
+        const message = validate(email.current.value, password.current.value);
+        console.log(message);
 
     }
 
@@ -19,7 +35,8 @@ const Login = () =>
             </div>
 
 
-      <form className="w-3/12 absolute p-16 bg-black bg-opacity-60 my-36 mx-auto right-0 left-0 text-white rounded-lg">
+      <form onSubmit={(e) => e.preventDefault()}
+      className="w-3/12 absolute p-16 bg-black bg-opacity-60 my-36 mx-auto right-0 left-0 text-white rounded-lg">
 
         {/* Sign In form */}
         <h1 className="font-bold text-3xl my-2 p-3">{isSignInForm ? "Sign In": "Sign Up" }</h1>
@@ -27,11 +44,18 @@ const Login = () =>
         {!isSignInForm && (
         <input type="text" placeholder="Name" className="p-4 my-4 w-full bg-slate-700 rounded-lg" />
         )}
-        
-        <input type="text" placeholder="Email-address" className="p-4 my-4 w-full bg-slate-700 rounded-lg" />
-        <input type="password" placeholder="Password" className="p-4 my-4 w-full bg-slate-700 rounded-lg" />
 
-        <button className="p-4 my-6 bg-rose-700 rounded-lg font-semibold w-full hover:bg-rose-500">{isSignInForm ? "Sign In": "Sign Up" }</button>
+        <input 
+        ref = {email}
+        type="text" placeholder="Email-address" className="p-4 my-4 w-full bg-slate-700 rounded-lg" />
+        
+        <input
+        ref = {password}
+         type="password" placeholder="Password" className="p-4 my-4 w-full bg-slate-700 rounded-lg" />
+
+        <button className="p-4 my-6 bg-rose-700 rounded-lg font-semibold w-full hover:bg-rose-500"
+            onClick={handleButtonClick}>
+             {isSignInForm ? "Sign In": "Sign Up" }</button>
 
         {/* SIGN UP FORM  */}
 
