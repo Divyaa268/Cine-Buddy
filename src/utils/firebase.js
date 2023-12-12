c923534455a3e7ac62700351fcc00aaee0cd76a6
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-12-12 02:29:42 -0500

    Created After firebase setup
    Created After firebase setup

---
+++ src/utils/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCB1wIqYo360nXCK_AeHl8utUQL0UfMQpE",
+  authDomain: "cinebuddy-a4829.firebaseapp.com",
+  projectId: "cinebuddy-a4829",
+  storageBucket: "cinebuddy-a4829.appspot.com",
+  messagingSenderId: "788993550903",
+  appId: "1:788993550903:web:6bc8178d2df0bcb3cfadb2",
+  measurementId: "G-WQ541QES7K"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
