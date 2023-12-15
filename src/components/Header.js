29ed35eb3b287eb9fe4268e6086cb449fad1fcee
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-12-15 03:00:26 -0500

    Increased width of header to whole screen
    Increased width of header to whole screen

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import img2 from '../cblogo11.png'
 
 const Header = () => {
   return (
-    <div className="absolute px-6 py-2 bg-gradient-to-b from-black z-10">
+    <div className="absolute px-6 py-2 w-screen bg-gradient-to-b from-black z-10">
       
       <img className="w-52"
       src={img2} alt="logo" />
