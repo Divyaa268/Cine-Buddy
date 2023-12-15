8788db0f040eceb34936cb8322c8ca7d5aae20e6
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-12-15 02:58:18 -0500

    dispatched add and remove user action
    dispatched add and remove user action

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,8 +5,12 @@ import { createBrowserRouter } from 'react-router-dom'
 import { RouterProvider } from 'react-router-dom'
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from '../utils/firebase'
+import { useDispatch } from 'react-redux';
+import { addUser, removeUser } from '../utils/userSlice'
 
 const Body = () => {
+
+  const dispatch = useDispatch();
 
     const appRouter = createBrowserRouter(
         [
@@ -24,13 +28,15 @@ const Body = () => {
     useEffect(() => {
       onAuthStateChanged(auth, (user) => {
         if (user) {
-          // User is signed in, see docs for a list of available properties
-          // https://firebase.google.com/docs/reference/js/auth.user
-          const uid = user.uid;
-          // ...
+          const { uid, email, displayName } = user;
+          // const uid = user.uid;
+          dispatch(addUser({ uid: uid, email:email, displayName:displayName}));
+          
         } else {
           // User is signed out
-          // ...
+          dispatch(removeUser());
+          // navigate("/");
+          
         }
       });
     }, 
