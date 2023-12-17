8e0e9af4dec75023b4b5c4ce27de8da4679a1276
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-12-17 02:25:46 -0500

    Created movieSlice and added to store
    Created movieSlice and added to store

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,30 +1,11 @@
 import React, { useEffect } from 'react'
 import Header from './Header'
-import { API_OPTIONS } from '../utils/constants';
+import useNowPlayingMovies from '../hooks/useNowPlayingMovies'
+
 
 const Browse = () => {
 
-  // WE CAN WRITE LIKE THIS AS WELL
-  // fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_OPTIONS)
-  // .then(response => response.json())
-  // .then(response => console.log(response))
-  // .catch(err => console.error(err));
-
-  const getNowPlayingMovies = async () => {
-    const data = await fetch (
-      'https://api.themoviedb.org/3/movie/now_playing?page=1',
-      API_OPTIONS
-    );
-    // we receive a promise from above so converting it to json
-    const json = await data.json();
-    console.log(json.results);
-    };
-
-    // using hook to call the api
-    useEffect(() => {
-      getNowPlayingMovies();
-    }, [])
-
+useNowPlayingMovies();
 
   return (
     <div>
