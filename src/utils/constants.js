66fd18536fd788844993fab3f6f1eed8dc9e5f3c
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-12-17 01:25:23 -0500

    NoePlayingMovie API
    NoePlayingMovie API

---
+++ src/utils/constants.js
@@ -0,0 +1,12 @@
+// // export const LOGO = '../cblogo11.png';
+// export const LOGO = 'src/cblogo11.png';
+// export const USER_LOGO = '../popcornFace.png'; 
+
+
+export const API_OPTIONS = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: "Bearer " + "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2YzZhMjg2OTZhYjJlZGI1MGM5YjQzMzczMDJiNzk3NCIsInN1YiI6IjY1N2UyZDdkOTA0ZjZkMDcyNDdmMDhlNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1Zs2BhHLjYWCE7ut-6Zwz4-Jh33FjRfYg-JK4V_nMZs",
+    },
+  };
