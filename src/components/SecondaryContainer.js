c18830b11e6dd1f6c7db86850750518b040175e4
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-12-18 04:03:14 -0500

    Added more lists
    Added more lists

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -7,10 +7,22 @@ const SecondaryContainer = () => {
   const movies = useSelector(store => store.movies)
 
   return (
-    <div>
-      <MovieList title = {"Now Playing"} movies = {movies.nowPlaying} />
+    movies.nowPlayingMovies && (
+    <div className='bg-black bg-gradient-to-l from-black to-red-950 w-screen'>
+    <div className='-mt-52 pl-2 relative z-20'> 
+      <MovieList title = {"Now Playing"} movies = {movies.nowPlayingMovies} />
+      <MovieList title={"Trending"} movies={movies.nowPlayingMovies} />
+      <MovieList title={"Popular"} movies={movies.popularMovies} />
+      <MovieList
+            title={"Upcoming Movies"}
+            movies={movies.nowPlayingMovies}
+          />
+      <MovieList title={"Horror"} movies={movies.nowPlayingMovies} />
+    </div>
     </div>
   )
+  )
 }
+
 
 export default SecondaryContainer
