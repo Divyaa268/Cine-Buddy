3620ddf37655697d8592a13773f4dfdeba856281
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-12-18 01:39:36 -0500

    Created MovieCard and MovieList components
    Created MovieCard and MovieList components

---
+++ src/components/MovieList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import MovieCard from './MovieCard'
+
+const MovieList = () => {
+  return (
+    <div>
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+      
+    </div>
+  )
+}
+
+export default MovieList
