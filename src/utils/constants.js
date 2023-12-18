b33f6fa8d51d4ea2c4d64c92cb72482ac14a8f16
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-12-18 04:04:02 -0500

    Used Image CDN Url
    Used Image CDN Url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -10,3 +10,5 @@ export const API_OPTIONS = {
       Authorization: "Bearer " + "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2YzZhMjg2OTZhYjJlZGI1MGM5YjQzMzczMDJiNzk3NCIsInN1YiI6IjY1N2UyZDdkOTA0ZjZkMDcyNDdmMDhlNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1Zs2BhHLjYWCE7ut-6Zwz4-Jh33FjRfYg-JK4V_nMZs",
     },
   };
+
+  export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
