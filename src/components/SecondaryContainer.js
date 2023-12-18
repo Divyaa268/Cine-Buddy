f715b814a52cb54954b06741416a8924fa298835
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-12-18 01:40:08 -0500

    Added MovieList component
    Added MovieList component

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import MovieList from './MovieList'
+import { useSelector } from 'react-redux'
 
 const SecondaryContainer = () => {
+
+  const movies = useSelector(store => store.movies)
+
   return (
     <div>
-      
+      <MovieList title = {"Now Playing"} movies = {movies.nowPlaying} />
     </div>
   )
 }
